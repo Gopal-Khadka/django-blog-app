321fc2c11c5d847633c8682b848b0fc36f937b26
Gopal Khadka <khadkagopal.mail@gmail.com>
2024-09-14 19:18:23 +0545

    Create basic html files and related views

--- backend/blogs/urls.py
+++ backend/blogs/urls.py
@@ -2,6 +2,16 @@ from django.urls import path
 
 from . import views
 
+app_name = "blogs"
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("home/", views.index, name="home"),
+    path("contact/", views.contact, name="contact"),
+    path("login/", views.logInUser, name="login"),
+    path("register/", views.register, name="register"),
+    path("logout/", views.logOutUser, name="logout"),
+    path("about/", views.about, name="about"),
+    path("blogs/", views.blogs, name="blogs"),
+    path("discussion/", views.discussion, name="discussion"),
 ]
--- backend/blogs/views.py
+++ backend/blogs/views.py
@@ -1,5 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
 def index(request):
-    return render(request, 'blogs/index.html')
+    return render(request, "blogs/index.html")
+
+
+def contact(request):
+    return render(request, "blogs/contact.html")
+
+
+def about(request):
+    return render(request, "blogs/about.html")
+
+
+def blogs(request):
+    return render(request, "blogs/blogs.html")
+
+
+@login_required(login_url="blogs:login")
+def discussion(request):
+    return render(request, "blogs/discussion.html")
+
+
+def logInUser(request):
+    if request.user.is_authenticated:
+        return redirect("blogs:index")
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(username=username, password=password)
+        print(user)
+        if user is not None:
+            login(request, user=user)
+            return redirect("blogs:index")
+        else:
+            return redirect("blogs:login")
+    return render(request, "blogs/login.html")
+
+
+def register(request):
+    return render(request, "blogs/register.html")
+
+
+def logOutUser(request):
+    logout(request)
+    return redirect("blogs:index")
